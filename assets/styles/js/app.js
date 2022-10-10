41fddbd5b8fbc3b7e8278721d55c4b3740a98ce8
Silverfabien <hollebeque.fabien@hotmail.com>
2022-10-10 18:26:47 +0200

    Add boostrap css and js

--- assets/styles/js/app.js
+++ assets/styles/js/app.js
@@ -10,3 +10,11 @@ import '../css/app.scss';
 
 // start the Stimulus application
 import '../../bootstrap';
+
+const $ = require('jquery');
+
+require('bootstrap');
+
+$(document).ready(function() {
+    $('[data-toggle="popover"]').popover();
+});
