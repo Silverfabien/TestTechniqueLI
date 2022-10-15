7eee213d77846c3e64280e1286c820b7953a87ef
Silverfabien <hollebeque.fabien@hotmail.com>
2022-10-15 18:07:55 +0200

    Addition of the repository + classification of users by their country then their region

--- src/Controller/Administration/UserController.php
+++ src/Controller/Administration/UserController.php
@@ -4,9 +4,7 @@ namespace App\Controller\Administration;
 
 use App\Entity\Security\User;
 use App\Form\Security\UserType;
-use App\Repository\Security\UserRepository;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use App\Repository\Administration\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -15,23 +13,37 @@ use Symfony\Component\Routing\Annotation\Route;
 #[Route('/admin')]
 class UserController extends AbstractController
 {
+    private UserRepository $userRepository;
+
+    /**
+     * @param UserRepository $userRepository
+     */
+    public function __construct(UserRepository $userRepository)
+    {
+        $this->userRepository = $userRepository;
+    }
+
+    /**
+     * @param UserRepository $userRepository
+     * @return Response
+     */
     #[Route('/', name: 'app_administration_user_index', methods: ['GET'])]
-    public function index(UserRepository $userRepository): Response
+    public function index(): Response
     {
         return $this->render('administration/user/index.html.twig', [
-            'users' => $userRepository->findAll(),
+            'users' => $this->userRepository->findBy([], ["country" => "ASC", "region" => "ASC"]),
         ]);
     }
 
     #[Route('/new', name: 'app_administration_user_new', methods: ['GET', 'POST'])]
-    public function new(Request $request, UserRepository $userRepository): Response
+    public function new(Request $request): Response
     {
         $user = new User();
         $form = $this->createForm(UserType::class, $user);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            $userRepository->save($user, true);
+            $this->userRepository->save($user, true);
 
             return $this->redirectToRoute('app_administration_user_index', [], Response::HTTP_SEE_OTHER);
         }
@@ -51,13 +63,16 @@ class UserController extends AbstractController
     }
 
     #[Route('/{id}/edit', name: 'app_administration_user_edit', methods: ['GET', 'POST'])]
-    public function edit(Request $request, User $user, UserRepository $userRepository): Response
+    public function edit(
+        Request $request,
+        User $user
+    ): Response
     {
         $form = $this->createForm(UserType::class, $user);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            $userRepository->save($user, true);
+            $this->userRepository->save($user, true);
 
             return $this->redirectToRoute('app_administration_user_index', [], Response::HTTP_SEE_OTHER);
         }
@@ -69,10 +84,13 @@ class UserController extends AbstractController
     }
 
     #[Route('/{id}', name: 'app_administration_user_delete', methods: ['POST'])]
-    public function delete(Request $request, User $user, UserRepository $userRepository): Response
+    public function delete(
+        Request $request,
+        User $user
+    ): Response
     {
         if ($this->isCsrfTokenValid('delete'.$user->getId(), $request->request->get('_token'))) {
-            $userRepository->remove($user, true);
+            $this->userRepository->remove($user, true);
         }
 
         return $this->redirectToRoute('app_administration_user_index', [], Response::HTTP_SEE_OTHER);
