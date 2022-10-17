6682707364547868c68ed0f238f77fd2d398bd63
Silverfabien <hollebeque.fabien@hotmail.com>
2022-10-17 16:59:03 +0200

    Using the API to retrieve countries and regions

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -22,65 +22,6 @@ class AppFixtures extends Fixture
 
     public function load(ObjectManager $manager): void
     {
-        $geolocationData = [
-            [
-                "country" => "Chine",
-                "regionName" => "Province de Guangdong"
-            ],
-            [
-                "country" => "États-Unis",
-                "regionName" => "Ohio"
-            ],
-            [
-                "country" => "États Unis",
-                "regionName" => "Géorgie"
-            ],
-            [
-                "country" => "États Unis",
-                "regionName" => "Massachusetts"
-            ],
-            [
-                "country" => "France",
-                "regionName" => "Île-de-France"
-            ],
-            [
-                "country" => "Italie",
-                "regionName" => "Lombardie"
-            ],
-            [
-                "country" => "Canada",
-                "regionName" => "Ontario"
-            ],
-            [
-                "country" => "Égypte",
-                "regionName" => "Alexandria"
-            ],
-            [
-                "country" => "Allemagne",
-                "regionName" => "Bavière"
-            ],
-            [
-                "country" => "Pays-Bas",
-                "regionName" => "Utrecht"
-            ],
-            [
-                "country" => "Corée du Sud",
-                "regionName" => "Daegu"
-            ],
-            [
-                "country" => "Espagne",
-                "regionName" => "Aragon"
-            ],
-            [
-                "country" => "Canada",
-                "regionName" => "Ontario"
-            ],
-            [
-                "country" => "Turquie",
-                "regionName" => "Istanbul"
-            ]
-        ];
-
         $jobData = [
             "Accompagnant", "Médecin", "Coach sportif", "Électricien", "Plombier", "Technicien", "Mécanicien",
             "Professeur", "Boulanger", "Boucher", "Pâtissier", "Développeur", "Chirurgien", "Vétérinaire"
@@ -88,22 +29,34 @@ class AppFixtures extends Fixture
 
         $faker = Faker\Factory::create('fr_FR');
 
-        for ($i = 0; $i<200; $i++) {
-            $geolocation = $geolocationData[random_int(0, count($geolocationData) - 1)];
+        for ($i = 0; $i<300; $i++) {
+            $ip = $faker->ipv4;
+            $ipApi = file_get_contents("http://ip-api.com/json/".$ip."?fields=9&lang=fr");
+            $decodeApi = json_decode($ipApi, true);
 
-            $user = new User();
-            $user->setFirstname($faker->firstName);
-            $user->setLastname($faker->lastName);
-            $user->setEmail($faker->email);
-            $user->setPassword($this->userPasswordHasher->hashPassword($user, $faker->password));
-            $user->setGender($faker->boolean());
+            // If API return null
+            if (!$decodeApi) {
+                $i--;
+            }
 
-            $user->setCountry($geolocation["country"]);
-            $user->setRegion($geolocation["regionName"]);
-            $user->setJob($jobData[random_int(0, count($jobData) - 1)]);
-            $user->setBirthday($faker->dateTimeBetween('-100 year'));
+            if ($decodeApi) {
+                $user = new User();
+                $user->setFirstname($faker->firstName);
+                $user->setLastname($faker->lastName);
+                $user->setEmail($faker->email);
+                $user->setPassword($this->userPasswordHasher->hashPassword($user, $faker->password));
+                $user->setGender($faker->boolean());
 
-            $manager->persist($user);
+                $user->setCountry($decodeApi["country"]);
+                $user->setRegion($decodeApi["regionName"]);
+                $user->setJob($jobData[random_int(0, count($jobData) - 1)]);
+                $user->setBirthday($faker->dateTimeBetween('-100 year'));
+
+                $manager->persist($user);
+            }
+
+            // Max 45 request per minute
+            sleep(2);
         }
 
         $manager->flush();
