d839441a237b8a886d4fac263fc435697b4448a5
Silverfabien <hollebeque.fabien@hotmail.com>
2022-10-21 20:07:20 +0200

    Adding responsive

--- assets/styles/js/admin/layouts/sidebar.js
+++ assets/styles/js/admin/layouts/sidebar.js
@@ -12,15 +12,56 @@ $(document).on("click", ".btn-submenu", function (e) {
 
 let sidebar = ".sidebar";
 let wrapper = ".wrapper";
+let title = ".title";
+let arrow = ".arrow";
+let item = ".dropdown-item";
 
 $(sidebar).on("mouseover", function () {
-    if ($(wrapper).hasClass("true-collapse")) {
-        $(wrapper).removeClass("is-collapsed");
+    if (
+        $(wrapper).hasClass("true-collapse") && $(wrapper).width() > 1439
+        || !$(wrapper).hasClass("true-collapse") && $(wrapper).width() < 1439 && $(wrapper).width() > 992
+    ) {
+        $(sidebar).css("width", "280px");
+        $(title).css("display", "inline-block");
+        $(arrow).css("opacity", 1);
+        $(item).css("display", "block")
     }
 })
 
 $(sidebar).on("mouseleave", function () {
-    if ($(wrapper).hasClass("true-collapse")) {
-        $(wrapper).addClass("is-collapsed");
+    if (
+        $(wrapper).hasClass("true-collapse") && $(wrapper).width() > 1439
+        || !$(wrapper).hasClass("true-collapse") && $(wrapper).width() < 1439 && $(wrapper).width() > 992
+    ) {
+        $(sidebar).removeAttr("style");
+        $(title).removeAttr("style");
+        $(arrow).removeAttr("style");
+        $(item).removeAttr("style")
     }
 })
+
+// $(sidebar).on("mouseover", function () {
+//     if (
+//         $(wrapper).hasClass("true-collapse") && $(wrapper).width() > 991
+//         || $(wrapper).width() < 1439 && $(wrapper).width() > 992
+//     ) {
+//         $(sidebar).css("width", "280px");
+//         $(title).css("display", "inline-block");
+//         $(arrow).css("opacity", 1);
+//         if ($(".nav-item").hasClass("active")) {
+//             console.log('ok')
+//             $(".active").children().next().css("display", "inline-block !important")
+//         }
+//     }
+// })
+//
+// $(sidebar).on("mouseleave", function () {
+//     if (
+//         $(wrapper).hasClass("true-collapse") && $(wrapper).width() > 991
+//         || $(wrapper).width() < 1439 && $(wrapper).width() > 992
+//     ) {
+//         $(sidebar).removeAttr("style");
+//         $(title).removeAttr("style");
+//         $(arrow).removeAttr("style");
+//     }
+// })
