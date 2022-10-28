e8b7a92f1ed829c2f1b7b7eb4354b2905b8caf19
Silverfabien <hollebeque.fabien@hotmail.com>
2022-10-28 17:06:06 +0200

    Index page formatting + navigation bar update

--- assets/styles/js/layouts/navbar.js
+++ assets/styles/js/layouts/navbar.js
@@ -2,3 +2,11 @@ $('.nav-trigger').click(function () {
     $(this).toggleClass('active');
     $(".collapse").toggleClass("show");
 });
+
+$(window).scroll(function () {
+    if ($(document).scrollTop() > 50) {
+        $('.nav-scroll').removeClass('navbarDefault');
+    } else {
+        $('.nav-scroll').addClass('navbarDefault');
+    }
+});
